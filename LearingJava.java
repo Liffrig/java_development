f2df1cc272a4f4df370461ec0a585eda9fa4d42d
Liffrig <michal.pavlovs@gmail.com>
2022-09-05 20:48:23 +0200

    Initial commit, java for learning

---
+++ LearingJava.java
@@ -0,0 +1,24 @@
+public class LearingJava {
+    public static int points = 0;
+    public static void main(String[] args) {
+    
+
+        
+
+    }
+
+
+    public static void Print(int input){
+        System.out.println(input);
+    }
+
+    public static void UpdatePoints(int amount){
+        points += amount;
+        Print(points);
+    }
+
+
+
+
+}
+
