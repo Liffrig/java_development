3ad9437ec9e9892ba4ead3c60dcd336b64341b6d
Liffrig <michal.pavlovs@gmail.com>
2022-09-07 18:24:21 +0200

    Stop at 4.3

--- LearingJava.java
+++ LearingJava.java
@@ -1,16 +1,9 @@
 public class LearingJava {
     public static void main(String[] args) {
-        String valueX = "test";
-        String valueY = "Test";
-        String valueZ = "Test";
-
-
-        System.out.println(valueX == valueY);
-        System.out.println(valueY == valueZ);
-
-
+       
 
 
     }
+
 
 }
