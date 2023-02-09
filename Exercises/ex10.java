9b1ba5b5890c39e270fec90364f33c4cc0df12b0
Liffrig <michal.pavlovs@gmail.com>
2023-02-09 01:11:21 +0100

    ex10 done

--- Exercises/ex10.java
+++ Exercises/ex10.java
@@ -1,4 +1,7 @@
 package Exercises;
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+
 import PPJLabs.Assistant;
 
 public class ex10 {
@@ -34,21 +37,50 @@ public class ex10 {
         KulaW kulaW_szescianie = new KulaW(kwadrat);
         KulaW kulaW_walcu = new KulaW(walec);
 
+        System.out.println(kulaW_szescianie.getR() + " -> promień kuli");
         System.out.println("kulaW_szescianie V: " + kulaW_szescianie.calculateV());
+
+        System.out.println(kulaW_walcu.getR() + " -> promień kuli");
         System.out.println("kulaW_walcu V: "+ kulaW_walcu.calculateV());
 
         assistant.printSeparator("Zadanie 6");
 
+        KulaNa kulaNaWalcu = new KulaNa(walec);
+        KulaNa kulaNaSzescianie = new KulaNa(kwadrat);
 
+        kulaNaWalcu.show();
+        kulaNaSzescianie.show();
 
+        assistant.printSeparator("Zadanie 7 & 8");
 
+        Donkey donkey = new Donkey(262);
 
-
-
-
+        // assumption
+        int initialBallons = (int) ((donkey.getMass() * 1000) / 6);
         
+        for (int i = 1; i < initialBallons; i++) {
+            Balloon b = new Balloon();
+            donkey.addBalloon(b); 
+        }
+
+        while (!donkey.isFlying()){
+
+            Balloon b = new Balloon();
+            donkey.addBalloon(b);
+
+        }
+
+        System.out.println("Przyczepione balony -> " + donkey.getBalloonsCount());
+        System.out.println("Ja latam!!!");
         
-        
+    }
+
+    public static double roundf(double value, int places) {
+        if (places < 0) throw new IllegalArgumentException();
+    
+        BigDecimal bd = new BigDecimal(Double.toString(value));
+        bd = bd.setScale(places, RoundingMode.HALF_UP);
+        return bd.doubleValue();
     }
 
 }
@@ -87,16 +119,17 @@ class Fruit{
 
 class Kwadrat{
     private double bok;
+    private double polePowierzchni;
 
     public Kwadrat(double bok) {
         this.bok = bok;
+        this.polePowierzchni = Math.pow(bok, 2);
     }
 
     public void show(){
-        double polePowierzchni = Math.pow(bok, 2);
-        double vSzescianu = polePowierzchni * bok;
+        double vSzescianu = this.polePowierzchni * bok;
 
-        System.out.println("Pole powierzchni kwadratu: " + polePowierzchni );
+        System.out.println("Pole powierzchni kwadratu: " + this.polePowierzchni);
         System.out.println("Objętość sześcianu o podstawie kwadratu: " + vSzescianu);
 
     }
@@ -104,59 +137,64 @@ class Kwadrat{
     public double getBok(){
         return this.bok;
     }
+
+    public double getPole(){
+        return polePowierzchni;
+    }
 }
 
 class Walec{
     private double r;
     private double h;
+    private double polePodstawy;
+    private double V;
 
     public Walec(double r, double h) {
         this.r = r;
         this.h = h;
+        this.polePodstawy = Math.PI * Math.pow(r, 2);
+        this.V = this.polePodstawy * this.h;
     }
 
     public void show(){
-        double polePodstawy = Math.PI * Math.pow(r, 2);
-        double objetosc = polePodstawy * this.h;
 
-        System.out.println("Pole podstawy walca: " + polePodstawy);
-        System.out.println("Objętość walca: " + objetosc);
+        System.out.println("Pole podstawy walca: " + this.polePodstawy);
+        System.out.println("Objętość walca: " + this.V);
 
     }
 
-    public double calculateV() {
-        double polePodstawy = Math.PI * Math.pow(r, 2);
-        double objetosc = polePodstawy * this.h;
-        return objetosc;
+    public double getPolePodstawy() {
+        return polePodstawy;
     }
+    public double getH() {
+        return h;
+    }
+    public double getR() {
+        return r;
+    }
+    public double getV() {
+        return V;
+    }
+
+
 
 }
 
 class KulaW{
     private double r;
 
-
-
     public KulaW(Walec walec) {
-        
-        double R = Math.pow(
-            (
-                (3 * walec.calculateV()) / (4 * Math.PI)), 
-            (1.0/3.0)
-            ) ;
 
-        this.r = R;
+        this.r = walec.getH() / 2; 
+        
+       
     }
 
     public KulaW(Kwadrat kwadrat) {
 
-        double R = Math.pow(
-            (
-                (3 * Math.pow(kwadrat.getBok(), 3)) / (4 * Math.PI)), 
-            (1.0/3.0)
-            ) ;
+        this.r = kwadrat.getBok() / 2;
 
-        this.r = R;
+      
     }
 
     public double calculateV(){
@@ -165,12 +203,100 @@ class KulaW{
         return v;
     }
 
+    public double getR() {
+        return this.r;
+    }
+
 
 
 }
 
 class KulaNa{
+
+    private double r;
+
+    public KulaNa(Walec walec) {
+        
+        double csquared = (Math.pow((2 * walec.getR()),2) + Math.pow(walec.getH(),2));
+        this.r = Math.sqrt(csquared);
+    }
+
+    public KulaNa(Kwadrat kwadrat){
+        this.r = (kwadrat.getBok() * Math.sqrt(2.0)) / 2;
+
+    }
+
+    public void show(){
+
+        System.out.println("Promień kuli wynosi " + r);
+    }
     
+
+}
+
+class Balloon{
+    double helium;
+
+    public Balloon(){
+        Assistant assistant = new Assistant();
+        this.helium = ex10.roundf( (0.005 + assistant.getRandomDouble(0.004)),3);
+    }
+
+    public double getLoad(){
+        return  ex10.roundf((this.helium / 0.007),4) * 6; // 0,007 -> 6g
+    }
+
+    public double getHelium() {
+        return helium;
+    }
+}
+
+class Donkey{
+    private double mass;
+    private Balloon[] baloons;
+
+    public Donkey(double mass){
+        this.mass = mass;
+        this.baloons = new Balloon[0];
+    }
+
+    public void addBalloon(Balloon balloon){
+        
+        Balloon[] newBalloonArr = new Balloon[this.baloons.length + 1];
+
+        for (int i = 0; i < this.baloons.length; i++) {
+            newBalloonArr[i] = this.baloons[i];
+        }
+        newBalloonArr[newBalloonArr.length -1] = balloon;
+        this.baloons = newBalloonArr;
+
+    }
+
+    public boolean isFlying(){
+        double massInGrams = this.mass * 1000;
+        double totalLoad = 0d;
+
+        for (int i = 0; i < this.baloons.length; i++) {
+            totalLoad += baloons[i].getLoad();
+
+        }
+
+        return totalLoad > massInGrams;
+
+    }
+
+    public int getBalloonsCount(){
+        return this.baloons.length;
+    }
+    
+    public double getMass() {
+        return mass;
+    }
+
+
+
+
+
 
 }
 
